25cc8d9deecd6243308abaec17eef7316707762b
Magrfs <magnolirfsit@gmail.com>
2020-04-27 14:12:24 +0900

    Add upload

--- project_garam/settings.py
+++ project_garam/settings.py
@@ -38,9 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders',
     'user',
     'branch',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
