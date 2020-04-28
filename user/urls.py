6893e80bf645e1878bdb9cdbe43fbc95c8b82191
Magrfs <magnolirfsit@gmail.com>
2020-04-28 21:17:52 +0900

    Fix all

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import UserView
+from .views import UserView, UserCheckView, SignInView
 
 urlpatterns = [
-    path('/member/join', UserView.as_view()),
+    path('sign-up', UserView.as_view()),
+    path('sign-up/check', UserCheckView.as_view()),
+    path('sign-in', SignInView.as_view()),
 ]
--- user/views.py
+++ user/views.py
@@ -1,61 +1,81 @@
-import json
+mport json
 import bcrypt
 import jwt
 import re
 
 from django.views import View
 from django.http import HttpResponse, JsonResponse
+from django.db import IntegrityError
 
 from .models import User
-from project_garam.settings import SECRET_KEY
 
 
-class UserView():
+class UserCheckView(View):
     def post(self, request):
         data = json.loads(request.body)
         try:
-            if len(json.keys()) == 1:
-                # 공백 \s
-                # 숫자 \d
-                # 한글 ㄱ-ㅎ가-힣ㅏ-ㅣ
-                # &$%
-                # 아이디에 한글 또는 특수문자(&$%)/공백은 사용할수 없습니다.
-                if len(re.findall(r"[ㄱ-ㅎ가-힣ㅏ-ㅣ&$%\s]", data['user_id'])) > 0:
-                    return JsonResponse({"message": "INVALID_ID"}, status=400)
+            if len(re.findall(r"[ㄱ-ㅎ가-힣ㅏ-ㅣ&$ %\s]", data['user_id'])) > 0 or len(data['user_id']) == 0:
+                return JsonResponse({"message": "INVALID_ID"}, status=400)
 
-                if User.objects.filter(user_id=data['user_id']).exists():
-                    return JsonResponse({"message": "DUPLICATE_ID"}, status=409)
-                return JsonResponse({"message": "ID_CHECK_COMPLETE"}, status=200)
+            if User.objects.filter(user_id=data['user_id']).exists():
+                return JsonResponse({"message": "DUPLICATE_ID"}, status=409)
+            return HttpResponse(status=200)
 
-            # 영문자/숫자/특수 문자가 모두 포함되어야 합니다. 사용 가능한 특수 문자는 !@.#^* ?+=_~ 입니다. 비밀번호는 최소 8자리 이상입니다. 공백은 사용하실 수 없습니다.
-            if len(re.findall(r"[^A-Za-z0-9!@.#^* ?+=_~]", data['password'])) > 0 or len(data['password']) < 8:
-                return JsonResponse({"message": "INVALID_PASSWORD"}, status=400)
+        except KeyError:
+            return JsonResponse({"message": "INVALID_KEYS"}, status=400)
 
-            hashed_password = bcrypt.hashpw(
-                data['password'].encode('utf-8'), bcrypt.gensalt())
+class UserView(View):
+    VALIDATION_RULES = {
+        'user_id': lambda user_id: False if User.objects.filter(user_id=user_id).exists() else True,
+        'password': lambda password: False if not re.match(r"^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@.#^* ?+=_~])[A-Za-z\d!@.#^* ?+=_~]{8,}$", password) else True,
+        'name': lambda name: False if name is None or len(name) == 0 else True,
+        'birth_date': lambda birth_date: False if not re.match(r"(\d{4})-(\d{2})-(\d{2})", birth_date) else True,
+        'phone': lambda phone: False if User.objects.filter(phone=phone).exists() else True,
+        'email': lambda email: False if not re.match(r"^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$", email) else True
+    }
 
-            if User.objects.fileter(phone=data['phone']).exists():
-                return JsonResponse({"message": "DUPLICATE_PHONE_NUMBER"}, status=400)
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+            if len(data.keys()) < 6:
+                return HttpResponse(status=400)
+            for value in data.values():
+                if value in "":
+                    return HttpResponse(status=400)
 
-            # if data['name'] == NULL:
-            #     return JsonResponse({"message": "이름내놔"}, status=400)
-
-            # if data['birthday'] = :
-            #     return JsonResponse({"message": "생일내놔"}, status=400)
-
-            # if data['phone'] = NULL:
-            #     return JsonResponse({"message": "전화번호내놔"}, status=400)
+            for field, validator in self.VALIDATION_RULES.items():
+                if not validator(data[field]):
+                    return HttpResponse(status=400)
 
             User.objects.create(
-                user_id=data['user_id']
-                password=data['user_id']
-                name=data['name']
-                birthday=data['birthday']
-                phone=data['phone']
-                email=data['email']
-                address=data['address']
+                user_id=data['user_id'],
+                password=bcrypt.hashpw(data['password'].encode('utf-8'), bcrypt.gensalt()).decode('utf-8'),
+                name=data['name'],
+                birth_date=data['birth_date'],
+                phone=data['phone'],
+                email=data['email'],
+                address=data.get('address', None),
             )
             return HttpResponse(status=200)
 
-            except KeyError:
-                return JsonResponse({"message": "INVALID_KEYS"}, status=400)
+        except IntegrityError:
+            return JsonResponse({"message": "DUPLICATED_KEYS"}, status=400)
+        except KeyError:
+            return JsonResponse({"message": "INVALID_KEYS"}, status=400)
+
+class SignInView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        print(data)
+        try:
+            if User.objects.filter(user_id = data['user_id']).exists():
+                user = User.objects.get(user_id = data['user_id'])
+
+                if bcrypt.checkpw(data['password'].encode('utf-8'), user.password.encode('utf-8')):
+                    token = jwt.encode({'id': user.id}, SECRET_KEY, ALGORITHM).decode('utf-8')
+                    return JsonResponse({"token": token}, status=200)
+                return JsonResponse({"message": "INVALID_PASSWORD"},status=400)
+            return JsonResponse({"message": "INVALID_ID"}, status=400)
+
+        except KeyError:
+            return JsonResponse({"message": "INVALID_KEYS"}, status=400)
